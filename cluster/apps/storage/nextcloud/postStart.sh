7ae258c364519c1beceaaf10cf941f73971c496d
Mike Kao <publicmike@outlook.com>
2022-12-12 00:43:29 +0000

    nc25

--- cluster/apps/storage/nextcloud/postStart.sh
+++ cluster/apps/storage/nextcloud/postStart.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
 #chown -R www-data:www-data /var/www/data
-chown -R www-data:www-data /var/www/html
-chmod 770 /var/www/data
+#chown -R www-data:www-data /var/www/html
+#chmod 770 /var/www/data
+apk add ffmpeg
