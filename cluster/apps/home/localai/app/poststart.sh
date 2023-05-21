97258483b483c2eba41b2fbb0e15c8816d370927
Mike Kao <publicmike@outlook.com>
2023-05-21 05:18:49 +0000

    blah

--- cluster/apps/home/localai/app/poststart.sh
+++ cluster/apps/home/localai/app/poststart.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-curl http://localhost:8080/models/apply -H "Content-Type: application/json" -d '{
-     "url": "https://raw.githubusercontent.com/go-skynet/model-gallery/main/gpt4all-j.yaml",
-     "name": "gpt4all-j"
-   }'
+curl http://localhost:8080/models/apply -H "Content-Type: application/json" -d '{"url": "https://raw.githubusercontent.com/go-skynet/model-gallery/main/gpt4all-j.yaml","name": "gpt4all-j"}'
