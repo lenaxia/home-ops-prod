404c3206c6d52e3194fcbf286725d047bbd939bb
Mike Kao <publicmike@outlook.com>
2024-01-24 22:54:18 +0000

    magicmirror enable

---
+++ cluster/apps/home/magicmirror/app/config/install_modules.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define the list of modules to install. Replace these URLs with your actual module URLs.
+modules_to_install=(
+    "https://gitlab.com/khassel/MMM-RepoStats"
+    "https://github.com/jalibu/MMM-RAIN-MAP"
+)
+
+cd /opt/magic_mirror/modules
+
+# Loop through the list of modules and install each
+for module_url in "${modules_to_install[@]}"; do
+    echo "Installing $module_url"
+    git clone "$module_url"
+    folder=$(echo "$module_url" | sed -r 's|.*\/(.*)|\1|g' | xargs)
+    cd "$folder"
+    npm install --omit=dev || true
+    cd ..
+done
+
+ls -la
