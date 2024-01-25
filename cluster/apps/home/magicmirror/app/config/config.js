a7098c576a355109efc814ab4d6c7ecfcbab4e8b
Mike Kao <publicmike@outlook.com>
2024-01-25 01:12:02 +0000

    mm

--- cluster/apps/home/magicmirror/app/config/config.js
+++ cluster/apps/home/magicmirror/app/config/config.js
@@ -1,5 +1,6 @@
+let config = {
   language: "en",
-  //locale: "en-US",
+  locale: "en-US",
   address: "0.0.0.0",
   port: 8080,
   ipWhitelist: ["127.0.0.1", "10.43.0.0/16", "192.168.0.0/16"],
@@ -74,5 +75,5 @@
       }
     },
   ]
-
+}
 
