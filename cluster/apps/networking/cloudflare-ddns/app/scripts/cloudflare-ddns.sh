73ce9116a9b001efe988dc6bd5d8249d7ddc4151
Lenaxia <github@47north.lat>
2024-08-19 17:00:01 +0000

    cloudflare
    Signed-off-by: Lenaxia <github@47north.lat>

--- cluster/apps/networking/cloudflare-ddns/app/scripts/cloudflare-ddns.sh
+++ cluster/apps/networking/cloudflare-ddns/app/scripts/cloudflare-ddns.sh
@@ -26,6 +26,8 @@ pushover_result=$(curl -s \
 # Fetch Current External IP
 current_ipv4="$(curl -s https://ipv4.icanhazip.com/)" || error_exit "Failed to fetch current IPv4 address"
 
+log "Fetched current IP Address: $current_ipv4"
+
 # Fetch Cloudflare Zone ID
 zone_id=$(curl -s -X GET \
     "https://api.cloudflare.com/client/v4/zones?name=${CLOUDFLARE_DOMAIN}&status=active" \
@@ -34,6 +36,9 @@ zone_id=$(curl -s -X GET \
     -H "Content-Type: application/json" \
     | jq --raw-output ".result[0] | .id" || error_exit "Failed to fetch Cloudflare Zone ID")
 
+
+log "Fetched zone id: $zone_id"
+
 # Fetch Current DNS Record
 record_ipv4=$(curl -s -X GET \
     "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records?name=${CLOUDFLARE_DOMAIN}&type=A" \
@@ -41,15 +46,21 @@ record_ipv4=$(curl -s -X GET \
     -H "X-Auth-Key: ${CLOUDFLARE_TOKEN}" \
     -H "Content-Type: application/json" || error_exit "Failed to fetch current DNS record")
 
+log "Fechted ipv4 record $record_ipv4"
+
 old_ip4=$(echo "$record_ipv4" | jq --raw-output '.result[0] | .content' || error_exit "Failed to parse current DNS record")
+
+log "Fetched old IP $old_ip4 from record"
 
 # Compare IPs and Update if Different
 if [[ "${current_ipv4}" == "${old_ip4}" ]]; then
-    log "IP Address '${current_ipv4}' has not changed"
+    log "IP Address '${current_ipv4}' has not changed ${old_ipv4}"
     exit 0
 fi
 
 record_ipv4_identifier="$(echo "$record_ipv4" | jq --raw-output '.result[0] | .id' || error_exit "Failed to parse DNS record identifier")"
+
+log "Fetched ipv4 identifier $record_ipv4_identifier"
 
 # Update DNS Record
 update_ipv4=$(curl -s -X PUT \
@@ -58,6 +69,8 @@ update_ipv4=$(curl -s -X PUT \
     -H "X-Auth-Key: ${CLOUDFLARE_TOKEN}" \
     -H "Content-Type: application/json" \
     --data "{\"id\":\"${zone_id}\",\"type\":\"A\",\"proxied\":false,\"name\":\"${CLOUDFLARE_DOMAIN}\",\"content\":\"${current_ipv4}\"}" || error_exit "Failed to update DNS record")
+
+log "Update ipv4 result: $update_ipv4"
 
 if [[ "$(echo "$update_ipv4" | jq --raw-output '.success')" == "true" ]]; then
     log "Success - IP Address '${current_ipv4}' has been updated"
